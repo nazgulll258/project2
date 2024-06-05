2ec66bee9f713fecd3975112976dfca580df94d7
timurtttt <tima.eshankul@gmail.com>
2024-06-05 12:22:12 +0300

    project3

--- script.js
+++ script.js
@@ -8,99 +8,111 @@ const divOl = document.getElementById('ol-div');
 const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const button = document.getElementById('add-todo');
+const names = document.getElementById('name');
+const passwords = document.getElementById('password');
+const form = document.getElementById('form');
+const voiti = document.getElementById('voiti');
 
-const Signin = () => {
-    signIn.style.display = 'block';
-};
-SignBtn.addEventListener('click', Signin);
-
-const Exit = () => {
-    exitDiv.style.display = 'block';
-};
-exit.addEventListener('click', Exit);
-
-const Closer = () => {
-    signIn.style.display = 'none';
-};
-Close.addEventListener('click', Closer);
-
-const Exitt = () => {
-    exitDiv.style.display = 'none';
-};
-exitBtn.addEventListener('click', Exitt);
-
-const postRequest = async(e) => {
+let saveId = null;
+const register = async(e) => {
     e.preventDefault();
-    const input = todoInput.value;
-    if (!input) return;
-    const todo = { text: input };
-    try {
-        await axios.post(`https://665d9e48e88051d604078a02.mockapi.io/todo/api/todo`, todo);
-        todoInput.value = '';
-        getRequest();
-    } catch (error) {
-        console.error("Error posting todo:", error);
+    const name = names.value;
+    const password = passwords.value;
+    if (!name || !password) {
+        alert('Введите корректное имя и пароль');
+        names.value = '';
+        passwords.value = '';
+        return;  
     }
-};
-button.addEventListener('click', postRequest);
-
-const getRequest = async() => {
+    const users = { name: name, password: password };
     try {
-        const response = await axios.get('https://665d9e48e88051d604078a02.mockapi.io/todo/api/todo');
-        const todo = response.data;
+        await axios.post('https://665ee3fc1e9017dc16f1e8bb.mockapi.io/users', users);
+        names.value = '';
+        passwords.value = '';
+    } catch (error) {
+      console.error('ощибка'+error);
+    }
+}
+
+form.addEventListener('submit', register);
+
+const Vhood = async () => {
+    try {
+        const response = await axios.get('https://665ee3fc1e9017dc16f1e8bb.mockapi.io/users');
+        const data = response.data;
+        const user = data.find(user => user.name === names.value && user.password === passwords.value);
+        
+        if (user) {
+            alert('Вы успешно вошли');
+            currentUserId = user.id;
+            GetRequest(user.id);
+        } else {
+            alert('Неверное имя или пароль');
+        }
+    } catch (error) {
+     
+    }
+}
+
+voiti.addEventListener('click', Vhood);
+
+const GetRequest = async (id) => {
+    try {
+        const response = await axios.get(`https://665ee3fc1e9017dc16f1e8bb.mockapi.io/users/${id}/tasks`);
+        const tasks = response.data;
         divOl.textContent = '';
-        todo.map((todos) => {
+        tasks.forEach((task) => {
             const li = document.createElement('li');
-            const inp = document.createElement('input');
-            inp.type = 'checkbox';
-            inp.addEventListener('click', () => toggleTextDecoration(li));
-            li.textContent = todos.text;
             const delet = document.createElement('button');
-            const remove = document.createElement('button');
-            remove.textContent = 'Remove';
-            remove.addEventListener('click', () => UpdateReguest(todos.id));
-            delet.textContent = 'Delete';
-            delet.addEventListener('click', () => deleteRequest(todos.id));
-            delet.className = 'delet';
-            li.className = 'li';
-            li.appendChild(inp);
+            const update = document.createElement('button');
+            delet.textContent = 'delete';
+            delet.addEventListener('click', () => deleteRequest(id, task.id));
+            update.textContent = 'update';
+            update.addEventListener('click', () => UpdateRequest(id, task.id));
+            li.textContent = task.text;
             li.appendChild(delet);
-            li.appendChild(remove);
+            li.appendChild(update);
             divOl.appendChild(li);
         });
     } catch (error) {
-        console.error(error, 'error');
+        console.error("error", error);
     }
-};
+}
 
-window.addEventListener('DOMContentLoaded', getRequest);
-
-const deleteRequest = async(id) => {
-    try {
-        await axios.delete(`https://665d9e48e88051d604078a02.mockapi.io/todo/api/todo/${id}`);
-        getRequest();
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-const UpdateReguest = async(id) => {
+const postRequest = async (e) => {
+    e.preventDefault();
     const input = todoInput.value;
-    if (!input) return;
-    const todos = { text: input }
+    const todo = { text: input };
     try {
-        await axios.put(`https://665d9e48e88051d604078a02.mockapi.io/todo/api/todo/${id}`,todos );
-        getRequest();
-        todoInput.value = '';
+        if (currentUserId) {
+            await axios.post(`https://665ee3fc1e9017dc16f1e8bb.mockapi.io/users/${currentUserId}/tasks`, todo);
+            todoInput.value = '';
+            GetRequest(currentUserId);
+        }
     } catch (error) {
-        console.error(error);
+        console.error("error", error);
     }
-};
+}
 
-function toggleTextDecoration(element) {
-    if (element.style.textDecoration === 'line-through') {
-        element.style.textDecoration = 'none';
-    } else {
-        element.style.textDecoration = 'line-through';
+todoForm.addEventListener('submit', postRequest);
+
+const deleteRequest = async (userId, taskId) => {
+    try {
+        await axios.delete(`https://665ee3fc1e9017dc16f1e8bb.mockapi.io/users/${userId}/tasks/${taskId}`);
+        GetRequest(userId);
+    } catch (error) {
+        console.error("error", error);
+    }
+}
+
+const UpdateRequest = async (userId, taskId) => {
+    const input = todoInput.value;
+    const upd = { text: input };
+    try {
+        await axios.put(`https://665ee3fc1e9017dc16f1e8bb.mockapi.io/users/${userId}/tasks/${taskId}zz`, upd);
+        todoInput.value = '';
+        GetRequest(userId);
+    } catch (error) {
+        console.error("error", error);
     }
 }
